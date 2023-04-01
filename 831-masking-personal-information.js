262f0adb0fcfdd4c19d01fe5c4cbb15bc663a244
WitchElaina <msz13399173387@gmail.com>
2023-04-01 19:51:54 +0800

    Add codes

---
+++ 831-masking-personal-information.js
@@ -0,0 +1,36 @@
+/**
+ * @param {string} email
+ * @return {string}
+ */
+function maskEmail(email) {
+    let lower = email.toLowerCase().split('@');
+    return lower[0].split('')[0] + '*****' + lower[0].split('')[lower[0].length - 1] + '@' + lower[1];
+}
+
+/**
+ * @param {string} phoneNum
+ */
+function maskPhone(phoneNum) {
+    num = phoneNum.split(/[\+\(\)\- ]/).join('');
+    let ret = '***-***-' + num.substring(num.length - 4, num.length);
+    let countryNum = ''
+    for (let i = 0; i < num.length - 10; i++) {
+        if (i === 0)
+            countryNum += '+';
+        countryNum += '*';
+    }
+    return countryNum === '' ? ret : countryNum + '-' + ret;
+}
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var maskPII = function(s) {
+    // judge
+    for (let i = 1; i <= s.length; i++) {
+        if (s[s.length - i] === '@')
+            return maskEmail(s);
+    }
+    return maskPhone(s);
+};
